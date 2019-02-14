82e14aab0198076850263128c04bbccd6c51020f
armin.lizde <armin.lizde@outlook.com>
2019-02-14 11:14:44 +0100

    Added facade and service layers

---
+++ CompleteAutoService.Facades/Facades/IMailService.cs
@@ -0,0 +1,16 @@
+﻿namespace CompleteAutoService.Facades
+{
+    /// <summary>
+    /// facade for mail services
+    /// </summary>
+    public interface IMailService
+    {
+        /// <summary>
+        /// sends mail
+        /// </summary>
+        /// <param name="recipient">mail from the recipient</param>
+        /// <param name="subject">subject of the mail</param>
+        /// <param name="body">body of the mail</param>
+        void Send(string recipient, string subject, string body);
+    }
+}
